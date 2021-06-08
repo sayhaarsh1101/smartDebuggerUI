1b127d2500b096012b9f7f898e86bc5c736fa730
harsh5kumar <t-harsh5.kumar@ocltp.com>
2021-06-08 17:45:51 +0530

    home page initiated

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+/* 
+import styles from '../styles/Home.module.css'
+import Tables from './Table'
+
+const Layout = ({children}) => {
+    return (
+        <>
+        
+        <div className={styles.container}>
+        <main className={styles.main}>
+        
+        {children}
+
+        
+        </main> 
+        <Tables></Tables>  
+        </div>
+        </>
+    )
+}
+
+export default Layout
+ */
