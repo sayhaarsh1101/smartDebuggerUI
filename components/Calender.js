612384614f7f36d4b32a72861a4c037a5f5cb327
Kushwanth2000 <t-krishna.kalakoti@ocltp.com>
2021-06-16 11:14:39 +0530

    added page attributes like PageCount,DocCount

--- components/Calender.js
+++ components/Calender.js
@@ -9,25 +9,16 @@ const Calender = () => {
     const {calenderstate, setcalenderstate  } = useContext(multiStateContext);
     
     const handleCallback=(start,end)=>{
-
         setcalenderstate({...calenderstate,startDate:start._d,endDate:end._d})
-      
       }
 
       
 
     return (
         <div>
-      <DateRangePicker  onCallback={handleCallback}>
+      <DateRangePicker  onCallback={handleCallback}  >
           <input type="text" className="form-control col-4" />
         </DateRangePicker>
-        {/* <br />
-        <h4>
-          startDate: <small>{state?.start?.format('MM/DD/YYYY (dddd)')}</small>
-        </h4>
-        <h4>
-          endDate: <small>{state?.end?.format('MM/DD/YYYY (dddd)')}</small>
-        </h4>*/}
         </div> 
       );
 };
