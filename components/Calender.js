58053cd5ed1861f63379b05b1cf84e0d2100f8e8
Kushwanth2000 <t-krishna.kalakoti@ocltp.com>
2021-06-16 12:13:16 +0530

    added timefilter with Last 7 and 30 days for DateRangePicker

--- components/Calender.js
+++ components/Calender.js
@@ -4,6 +4,7 @@ import {multiStateContext} from './StateContext'
 import React, { Component , useState , useContext} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-daterangepicker/daterangepicker.css';
+
 const Calender = () => {
    
     const {calenderstate, setcalenderstate  } = useContext(multiStateContext);
@@ -16,7 +17,15 @@ const Calender = () => {
 
     return (
         <div>
-      <DateRangePicker  onCallback={handleCallback}  >
+      <DateRangePicker  onCallback={handleCallback} initialSettings={
+        { timePicker:true, 
+          locale: {format: 'DD/MM/YYYY hh:mm A'} , 
+          ranges: {'Today': [new Date(new Date().setHours(0,0,0,0)) , new Date()], 
+                  ' Last 7 Days': [new Date(new Date().setDate(new Date().getDate()-7)), new Date()],
+                  ' Last 30 Days': [new Date(new Date().setDate(new Date().getDate()-29)), new Date()],
+                   } 
+          }}>
+          
           <input type="text" className="form-control col-4" />
         </DateRangePicker>
         </div> 
